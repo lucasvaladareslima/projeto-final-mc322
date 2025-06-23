a37551b3e41d89aec3f48c73ff3c23ba06708e2f
lucas souza <gs10.lucas@gmail.com>
2025-06-23 20:31:49 -0300

    comentarios de views e testes de ensinos

--- backend/ensino/views/conteudo_turma_view.py
+++ backend/ensino/views/conteudo_turma_view.py
@@ -33,42 +33,61 @@ class PublicacaoListView(ListAPIView):
         return queryset
     
 class MaterialCreateView(CreateAPIView):
+    """View para criar materiais associados a uma turma."""
+
     serializer_class = MaterialSerializer
 
     def perform_create(self, serializer):
-        """Salva o material com o usuário autenticado como autor."""
+        """Salva o material com o usuário autenticado como autor.
+        Args:
+            serializer (MaterialSerializer): O serializer usado para validar e salvar o material.
+        """
         serializer.save(autor=self.request.user)
 
 class TarefaCreateView(CreateAPIView):
+    """View para criar tarefas associadas a uma turma."""
     serializer_class = TarefaSerializer
 
     def perform_create(self, serializer):
-        """Salva a tarefa com o usuário autenticado como autor."""
+        """Salva a tarefa com o usuário autenticado como autor.
+        Args:
+            serializer (TarefaSerializer): O serializer usado para validar e salvar a tarefa.
+        """
         serializer.save(autor=self.request.user)
 
 class AnuncioCreateView(CreateAPIView):
+    """View para criar anúncios associados a uma turma."""
     serializer_class = AnuncioSerializer
 
     def perform_create(self, serializer):
-        """Salva o anúncio com o usuário autenticado como autor."""
+        """Salva o anúncio com o usuário autenticado como autor.
+        Args:
+            serializer (AnuncioSerializer): O serializer usado para validar e salvar o anúncio.
+        """
         serializer.save(autor=self.request.user)
 
 
 class EntregaTarefaCreateView(CreateAPIView):
+    """View para criar entregas de tarefas associadas a uma tarefa específica."""
     serializer_class = EntregaTarefaSerializer
 
     def perform_create(self, serializer):
-        """Salva a entrega de tarefa com o usuário autenticado como aluno."""
+        """Salva a entrega de tarefa com o usuário autenticado como aluno.
+        Args:
+            serializer (EntregaTarefaSerializer): O serializer usado para validar e salvar a entrega de tarefa.
+        """
         serializer.save(aluno=self.request.user)
 
 class TarefasPendentesListView(ListAPIView):
+    """View para listar tarefas pendentes do usuário autenticado."""
     serializer_class = TarefaSerializer
 
     def get_queryset(self):
         """Obtém as entregas de tarefas pendentes do usuário autenticado.
         
         Filtra as entregas de tarefas onde o status é 'pendente' e o aluno é o usuário autenticado.
-        
+        Args:
+            request: Objeto de requisição HTTP que contém o usuário autenticado.
         Returns:
             QuerySet: Lista de entregas de tarefas pendentes do usuário autenticado.
         """
